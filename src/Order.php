dbc7bb24f0623fe2a4695870eaa375e6771f792f
Jagepard <dankorot@gmail.com>
2019-12-13 15:29:41 +0300

    Add Order

---
+++ src/Order.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @author  : Jagepard <jagepard@yandex.ru>
+ * @license https://mit-license.org/ MIT
+ */
+
+namespace Creational\Builder;
+
+final class Order
+{
+    const MB = 'X570';
+    const CPU = 'AMD Ryzen™ 7 3700X';
+    const RAM = 'DDR4 16Gb 3200MHz PC25600';
+    const GPU = 'GeForce RTX 2070 Super';
+    const SSD = '500Gb';
+    const HDD = '2Tb';
+}
