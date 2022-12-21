1c14c4b34a3fa4753c97a6c6c5aacb3e02fad473
Jagepard <dankorot@gmail.com>
2022-12-21 13:13:32 +0300

    remove getComputer

--- src/PCBuilder.php
+++ src/PCBuilder.php
@@ -15,14 +15,22 @@ class PCBuilder implements BuilderInterface
 {
     private array $components;
 
-    public function setPart(AbstractPart $part): BuilderInterface
+    public function createComputer(): ComputerInterface
+    {
+        $this
+            ->setPart(new Hardware\Motherboard(Order::MB))
+            ->setPart(new Hardware\Cpu(Order::CPU))
+            ->setPart(new Hardware\Ram(Order::RAM))
+            ->setPart(new Hardware\Gpu(Order::GPU))
+            ->setPart(new Hardware\Ssd(Order::SSD))
+            ->setPart(new Hardware\Hdd(Order::HDD));
+
+        return new Desktop($this->components);
+    }
+
+    private function setPart(AbstractPart $part): BuilderInterface
     {
         $this->components[get_class($part)] = $part;
         return $this;
-    }
-
-    public function getComputer(): ComputerInterface
-    {
-        return new Desktop($this->components);
     }
 }
