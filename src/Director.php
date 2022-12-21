931da066e0cab7893fa87f1d54551194cdd8d4ca
Jagepard <dankorot@gmail.com>
2022-12-21 13:13:05 +0300

    update build

--- src/Director.php
+++ src/Director.php
@@ -13,23 +13,8 @@ use Creational\Builder\Interfaces\{DirectorInterface, BuilderInterface, Computer
 
 class Director implements DirectorInterface
 {
-    private BuilderInterface $builder;
-
-    public function build(): ComputerInterface
+    public function build(BuilderInterface $builder): ComputerInterface
     {
-        $this->builder
-            ->setPart(new Hardware\Motherboard(Order::MB))
-            ->setPart(new Hardware\Cpu(Order::CPU))
-            ->setPart(new Hardware\Ram(Order::RAM))
-            ->setPart(new Hardware\Gpu(Order::GPU))
-            ->setPart(new Hardware\Ssd(Order::SSD))
-            ->setPart(new Hardware\Hdd(Order::HDD));
-
-        return $this->builder->getComputer();
-    }
-
-    public function setBuilder(BuilderInterface $builder): void
-    {
-        $this->builder = $builder;
+        return $builder->createComputer();
     }
 }
